101a5410783d488726c2590bdcc2b9eeff739e0b
Emile Swarts <emile.swarts123@gmail.com>
2020-07-29 12:00:53 +0100

    Update to integrate with database (#1)
    * WIP DB integration
    
    * Do the schema migration in main docker container
    
    This is idempotent, so do on each release
    Remove now unused config docker file

--- dhcp-service/start_service.sh
+++ dhcp-service/start_service.sh
@@ -7,4 +7,6 @@ sed -i "s/<DB_PASS>/$DB_PASS/g" /etc/kea/config.json
 sed -i "s/<DB_HOST>/$DB_HOST/g" /etc/kea/config.json
 sed -i "s/<DB_PORT>/$DB_PORT/g" /etc/kea/config.json
 
+kea-admin db-init mysql -u $DB_USER -p $DB_PASS -n $DB_NAME -h $DB_HOST
+cat /etc/kea/config.json
 kea-dhcp4 -c /etc/kea/config.json
