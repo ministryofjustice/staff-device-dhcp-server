ee7681c07e13547719754a9a10ffb88cf6c0d817
Andy Mitchell <me@themitchell.co.uk>
2020-08-17 11:21:40 +0100

    Start the KEA Control Agent and open http API ports

--- dhcp-service/start_service.sh
+++ dhcp-service/start_service.sh
@@ -10,4 +10,5 @@ sed -i "s/<DB_HOST>/$DB_HOST/g" /etc/kea/config.json
 sed -i "s/<DB_PORT>/$DB_PORT/g" /etc/kea/config.json
 
 kea-admin db-init mysql -u $DB_USER -p $DB_PASS -n $DB_NAME -h $DB_HOST > /dev/null
-kea-dhcp4 -c /etc/kea/config.json
+kea-ctrl-agent -c /etc/kea/kea-ctrl-agent.conf \
+& kea-dhcp4 -c /etc/kea/config.json
