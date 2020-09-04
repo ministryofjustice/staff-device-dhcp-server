8daf8ec469a3acfc082ba9e0a742b48344f4a1b8
Emile <emile.swarts@digital.justice.gov.uk>
2020-09-04 15:05:40 +0100

    Use local variables in deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,12 +4,14 @@ set -e
 
 outputs=${DHCP_DNS_TERRAFORM_OUTPUTS} | jq -r .ecs
 
-DHCP_SERVER_CLUSTER_NAME=`echo $outputs | jq .dhcp_cluster_name`
-DHCP_SERVER_SERVICE_NAME=`echo $outputs | jq .dhcp_service_name`
-DHCP_SERVER_TASK_DEFINITION_NAME=`echo $outputs | jq .dhcp_task_definition_name`
+dhcp_server_cluster_name=`echo $outputs | jq .dhcp_cluster_name`
+dhcp_server_service_name=`echo $outputs | jq .dhcp_service_name`
+dhcp_server_task_definition_name=`echo $outputs | jq .dhcp_task_definition_name`
+
+echo $dhcp_server_task_definition_name
 
 aws ecs update-service \
-  --cluster $DHCP_SERVER_CLUSTER_NAME \
-  --service $DHCP_SERVER_SERVICE_NAME \
-  --task-definition $DHCP_SERVER_TASK_DEFINITION_NAME  \
+  --cluster $dhcp_server_cluster_name \
+  --service $dhcp_server_service_name \
+  --task-definition $dhcp_server_task_definition_name \
   --force-new-deployment
