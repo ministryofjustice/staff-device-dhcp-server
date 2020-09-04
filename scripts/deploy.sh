0954586af3e73cab6fa2c43fe46ef7e0e98c3d22
Emile <emile.swarts@digital.justice.gov.uk>
2020-09-04 15:22:04 +0100

    Correct fetching from Terraform object

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,13 +2,11 @@
 
 set -e
 
-outputs=${DHCP_DNS_TERRAFORM_OUTPUTS} | jq -r .ecs
+outputs=${DHCP_DNS_TERRAFORM_OUTPUTS} | jq -r .dhcp | jq -r .ecs
 
-dhcp_server_cluster_name=`echo $outputs | jq .dhcp_cluster_name`
-dhcp_server_service_name=`echo $outputs | jq .dhcp_service_name`
-dhcp_server_task_definition_name=`echo $outputs | jq .dhcp_task_definition_name`
-
-echo $dhcp_server_task_definition_name
+dhcp_server_cluster_name=`echo $outputs | jq .cluster_name`
+dhcp_server_service_name=`echo $outputs | jq .service_name`
+dhcp_server_task_definition_name=`echo $outputs | jq .task_definition_name`
 
 aws ecs update-service \
   --cluster $dhcp_server_cluster_name \
