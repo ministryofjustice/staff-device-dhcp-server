c08aa1c1df2719cdeae29ac0d746265b39d45927
Emile <emile.swarts@digital.justice.gov.uk>
2020-09-04 16:22:27 +0100

    Assume role to target deployment account, currently targetted at default AWS account which is shared services

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,10 +7,24 @@
 
 set -e
 
-cluster_name=$( jq -r '.dhcp.ecs.cluster_name' <<< "${DHCP_DNS_TERRAFORM_OUTPUTS}" )
-service_name=$( jq -r '.dhcp.ecs.service_name' <<< "${DHCP_DNS_TERRAFORM_OUTPUTS}" )
+deploy() {
+  cluster_name=$( jq -r '.dhcp.ecs.cluster_name' <<< "${DHCP_DNS_TERRAFORM_OUTPUTS}" )
+  service_name=$( jq -r '.dhcp.ecs.service_name' <<< "${DHCP_DNS_TERRAFORM_OUTPUTS}" )
 
-aws ecs update-service \
-  --cluster $cluster_name \
-  --service $service_name \
-  --force-new-deployment
+  aws ecs update-service \
+    --cluster $cluster_name \
+    --service $service_name \
+    --force-new-deployment
+}
+
+assume_deploy_role() {
+  TEMP_ROLE=`aws sts assume-role --role-arn $ROLE_ARN --role-session-name ci-dhcp-deploy-$CODEBUILD_BUILD_NUMBER`
+  export AWS_ACCESS_KEY_ID=$(echo "${TEMP_ROLE}" | jq -r '.Credentials.AccessKeyId')
+  export AWS_SECRET_ACCESS_KEY=$(echo "${TEMP_ROLE}" | jq -r '.Credentials.SecretAccessKey')
+  export AWS_SESSION_TOKEN=$(echo "${TEMP_ROLE}" | jq -r '.Credentials.SessionToken')
+}
+
+main() {
+  assume_deploy_role
+  deploy
+}
