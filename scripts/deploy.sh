1af45b5073c042986599a6adc06740b727599b23
Emile <emile.swarts@digital.justice.gov.uk>
2020-09-04 15:58:39 +0100

    No need to specify task definition, latest active is used by default

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,10 +6,8 @@ output=$( jq -r '.dhcp.ecs' <<< "${DHCP_DNS_TERRAFORM_OUTPUTS}" )
 
 cluster_name=$( jq '.cluster_name' <<< "${output}" )
 service_name=$( jq '.service_name' <<< "${output}" )
-task_definition_name=$( jq '.task_definition_name' <<< "${output}" )
 
 aws ecs update-service \
   --cluster $cluster_name \
   --service $service_name \
-  --task-definition $task_definition_name \
   --force-new-deployment
