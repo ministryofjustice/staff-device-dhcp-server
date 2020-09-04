d02f5b1414e833af8f1a27e48bbced58671a39b5
Emile <emile.swarts@digital.justice.gov.uk>
2020-09-04 16:04:47 +0100

    Force raw output for jq
    This removes the quotes from the terraform output variables

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+# This deployment script starts a zero downtime phased deployment.
+# It works by doubling the currently running tasks by introducing the new versions
+# Auto scaling will detect that there are too many tasks running for the current load and slowly start decomissioning the old running tasks
+# Production traffic will gradually be moved to the new running tasks
+
+
 set -e
 
 output=$( jq -r '.dhcp.ecs' <<< "${DHCP_DNS_TERRAFORM_OUTPUTS}" )
 
-cluster_name=$( jq '.cluster_name' <<< "${output}" )
-service_name=$( jq '.service_name' <<< "${output}" )
+cluster_name=$( jq -r '.cluster_name' <<< "${output}" )
+service_name=$( jq -r '.service_name' <<< "${output}" )
 
-aws ecs update-service \
-  --cluster $cluster_name \
-  --service $service_name \
-  --force-new-deployment
+echo $service_name
+
+# aws ecs update-service \
+#   --cluster $cluster_name \
+#   --service $service_name \
+#   --force-new-deployment
