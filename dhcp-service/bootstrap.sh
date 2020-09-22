93bed54e0970635c0dd7ddeaf143f64b79503a87
Emile <emile.swarts@digital.justice.gov.uk>
2020-09-22 10:42:35 +0100

    Add debugging statement to test granting access to lease db

--- dhcp-service/bootstrap.sh
+++ dhcp-service/bootstrap.sh
@@ -33,6 +33,7 @@ init_schema_if_not_loaded() {
 }
 
 ensure_database_permissions() {
+  echo "running grants on lease db"
   mysql -u ${DB_USER} -p${DB_PASS} -n ${DB_NAME} -h ${DB_HOST} -e "GRANT ALL ON ${DB_NAME}.* TO '${DB_USER}'@'${DB_HOST}';" #https://kea.readthedocs.io/en/kea-1.6.3/arm/admin.html
 }
 
