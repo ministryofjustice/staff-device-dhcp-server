bc75b84608717700b79e953295604458e3dd9802
Emile <emile.swarts@digital.justice.gov.uk>
2020-11-11 16:55:26 +0000

    Make metrics API call every minute

--- dhcp-service/metrics/boot_metrics_agent.rb
+++ dhcp-service/metrics/boot_metrics_agent.rb
@@ -4,15 +4,16 @@ require "json"
 require_relative "publish_metrics"
 require_relative "aws_client"
 
-uri = URI.parse("http://localhost:8000/")
-
-req = Net::HTTP::Post.new(uri.path, "Content-Type" => "application/json")
-req.body = { command: "statistic-get-all", service: ["dhcp4"] }.to_json
-http = Net::HTTP.new(uri.host, uri.port)
-kea_stats = JSON.parse(http.request(req).body)
-
 while true do
+  uri = URI.parse("http://localhost:8000/")
+
+  req = Net::HTTP::Post.new(uri.path, "Content-Type" => "application/json")
+  req.body = { command: "statistic-get-all", service: ["dhcp4"] }.to_json
+  http = Net::HTTP.new(uri.host, uri.port)
+  kea_stats = JSON.parse(http.request(req).body)
+
   PublishMetrics.new(client: AwsClient.new).execute(kea_stats: kea_stats)
   p "publishing metrics: #{kea_stats}"
+
   sleep 60
 end
