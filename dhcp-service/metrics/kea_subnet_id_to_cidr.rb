f798ad0b4d6a35e63e30f24e1607f662fb259415
Emile <emile.swarts@digital.justice.gov.uk>
2020-12-07 15:14:37 +0000

    Cache the results of fetching the config
    This API call should only happen once, store result in memory

--- dhcp-service/metrics/kea_subnet_id_to_cidr.rb
+++ dhcp-service/metrics/kea_subnet_id_to_cidr.rb
@@ -4,12 +4,14 @@ class KeaSubnetIdToCidr
   end
 
   def execute(subnet_id:)
-    subnet_rows = kea_client.get_config[0]["arguments"]["Dhcp4"]["subnet4"]
-
-    subnet_rows.find { |row| subnet_id.to_s == row.fetch("id").to_s }["subnet"]
+    config.find { |row| subnet_id.to_s == row.fetch("id").to_s }["subnet"]
   end
 
   private
+
+  def config
+    @config ||= kea_client.get_config[0]["arguments"]["Dhcp4"]["subnet4"]
+  end
 
   attr_reader :kea_client
 end
