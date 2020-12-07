99dd70d90b635a7a9655aa4b465cbc48839bdec2
Emile <emile.swarts@digital.justice.gov.uk>
2020-12-07 17:18:21 +0000

    Remove debugging output
    No need to print as the subnet CIDR ranges are now showing up in Grafana

--- dhcp-service/metrics/kea_subnet_id_to_cidr.rb
+++ dhcp-service/metrics/kea_subnet_id_to_cidr.rb
@@ -4,24 +4,13 @@ class KeaSubnetIdToCidr
   end
 
   def execute(subnet_id:)
-    p "subnet id is: #{subnet_id}"
-
-    result = config.find do |row|
-      p "comparing: #{subnet_id.to_s} to #{row.fetch("id").to_s}"
-
-      subnet_id.to_s == row.fetch("id").to_s
-    end
-
-    p "row is: #{result}"
-    result["subnet"]
+    config.find { |row| subnet_id.to_s == row.fetch("id").to_s }["subnet"]
   end
 
   private
 
   def config
     @config ||= kea_client.get_config[0]["arguments"]["Dhcp4"]["subnet4"]
-    p @config
-    @config
   end
 
   attr_reader :kea_client
