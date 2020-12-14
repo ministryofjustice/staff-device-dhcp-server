ac2ed671e5899c5ff6dea6bb84773853db81b5f4
Emile <emile.swarts@digital.justice.gov.uk>
2020-12-14 15:28:05 +0000

    Add debugging to see if metrics are being published

--- dhcp-service/metrics/aws_client.rb
+++ dhcp-service/metrics/aws_client.rb
@@ -9,6 +9,7 @@ class AwsClient
   end
 
   def put_metric_data(metrics)
+    p "metrics slice: #{metrics_slice}"
     sliced(metrics).each do |metrics_slice|
       client.put_metric_data(
         namespace: "DHCP-Kea-Server",
