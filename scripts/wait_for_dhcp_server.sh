c02e68f464aa7ade5decd141ee7c8453adb516ce
Joshua-Luke Bevan <joshua.bevan@madetech.com>
2020-12-17 12:13:32 +0000

    Add -eou back to pipeline

--- scripts/wait_for_dhcp_server.sh
+++ scripts/wait_for_dhcp_server.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euo pipefail
+
 printf "Waiting for Primary KEA DHCP server"
 
 count=0
@@ -7,9 +9,9 @@ until docker-compose exec -T dhcp-primary ls /tmp/kea_started 2> /dev/null
 do
   printf "."
   sleep 1
-  let count++
+  (( count++ )) || true
 
-  if [ $count -ge 10 ]; then
+  if [ "$count" -ge 10 ]; then
     echo "Failed to start server"
     docker-compose logs
     exit 1
