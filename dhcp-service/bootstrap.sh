b77d77813982e20c28fa27d108c142bc1a3d4b68
Andy Mitchell <me@themitchell.co.uk>
2020-12-17 13:58:49 +0000

    Ensure credentials are substituted in reloaded config

--- dhcp-service/bootstrap.sh
+++ dhcp-service/bootstrap.sh
@@ -63,7 +63,24 @@ ensure_healthy_server() {
 }
 
 start_kea_config_reload_daemon(){
-  ./reload_config.sh &
+  if [ "$LOCAL_DEVELOPMENT" != "true" ]; then
+    while [ true ]; do
+      echo "Checking for new configurations..."
+      aws s3 sync s3://${KEA_CONFIG_BUCKET_NAME} /tmp/configurations --exclude "*" --include "config.json"
+      cmp -s /etc/kea/config.json /tmp/configurations/config.json
+
+      if [ $? -ne 0 ]; then
+        echo "Configuration changes detected. Copying in place and updating Kea"
+        cp /tmp/configurations/config.json /etc/kea/config.json
+        configure_database_credentials
+        curl -X "POST" "http://localhost:8000/" \
+            -H 'Content-Type: application/json' \
+            -d '{"command": "config-reload"}'
+      else
+        echo "No configuration changes detected"
+      fi
+    done
+  fi
 }
 
 main() {
