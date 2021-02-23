ba788e73fdabde1536858ac73a0241480028adee
Andy Mitchell <me@themitchell.co.uk>
2021-02-23 15:25:59 +0000

    Send metrics agent errors to Sentry

--- dhcp-service/metrics/boot_metrics_agent.rb
+++ dhcp-service/metrics/boot_metrics_agent.rb
@@ -1,6 +1,7 @@
 require 'aws-sdk-cloudwatch'
 require "net/http"
 require "json"
+require "sentry-ruby"
 require_relative "publish_metrics"
 require_relative "aws_client"
 require_relative "kea_client"
@@ -8,17 +9,45 @@ require_relative "db_client"
 require_relative "kea_lease_usage"
 require_relative "kea_subnet_id_to_cidr"
 
-kea_client = KeaClient.new
-db_client = DbClient.new
-kea_lease_usage = KeaLeaseUsage.new(kea_client: kea_client, db_client: db_client)
-
-while true do
-  kea_subnet_id_to_cidr = KeaSubnetIdToCidr.new(kea_client: kea_client)
-
-  PublishMetrics.new(
-    client: AwsClient.new,
-    kea_lease_usage: kea_lease_usage,
-    kea_subnet_id_to_cidr: kea_subnet_id_to_cidr
-  ).execute(kea_stats: kea_client.get_statistics)
-  sleep 10
+Sentry.init do |config|
+  # All errors will be sent syncronously!
+  config.background_worker_threads = 0
+  config.environment = ENV["SENTRY_CURRENT_ENV"]
 end
+
+class Agent
+  def execute
+    loop do
+      kea_subnet_id_to_cidr = KeaSubnetIdToCidr.new(kea_client: kea_client)
+
+      PublishMetrics.new(
+        client: AwsClient.new,
+        kea_lease_usage: kea_lease_usage,
+        kea_subnet_id_to_cidr: kea_subnet_id_to_cidr
+      ).execute(kea_stats: kea_client.get_statistics)
+      sleep 10
+    end
+  rescue => e
+    Sentry.capture_exception(e)
+    raise e
+  end
+
+  private
+
+  def kea_client
+    @kea_client ||= KeaClient.new
+  end
+
+  def db_client
+    @db_client ||= DbClient.new
+  end
+
+  def kea_lease_usage
+    @kea_lease_usage ||= KeaLeaseUsage.new(
+      kea_client: kea_client,
+      db_client: db_client
+    )
+  end
+end
+
+Agent.new.execute
