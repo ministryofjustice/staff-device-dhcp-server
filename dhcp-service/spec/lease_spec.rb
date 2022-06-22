fede4c518582c9872b4c14ca8be5d2282384a487
Touhidul Islam <touhidul.islam@justice.gov.uk>
2022-06-22 21:53:40 +0100

    :white_check_mark: add tests for client class options and global options

--- dhcp-service/spec/lease_spec.rb
+++ dhcp-service/spec/lease_spec.rb
@@ -2,18 +2,20 @@ require_relative "spec_helper"
 require_relative "../metrics/db_client"
 require 'sequel'
 
-describe "getting leases" do
+describe "Kea server" do
   let(:db_client) { DbClient.new.db }
+  let(:dhcp_offer_packet_content) { `tshark -r ./dhcp_offer_packet.pcap -V -T text` }
 
   before do
     db_client[:lease4].truncate
     db_client.disconnect
+    Process.fork { `tshark -iany -f 'ip src 172.1.0.10 and udp port 67' -w ./dhcp_offer_packet.pcap -q -a packets:1` }
   end
 
   after { db_client.disconnect }
 
-  context "when 10 ordinary cliens are requesting IPs" do
-    it "leases out 10 leases and leases persist in the DB" do
+  context "when ordinary dhcp clients send DHCP requests" do
+    it "provides 10 leases to 10 clients, leases persist in the DB and provides DHCP options from global options" do
       `perfdhcp -r 2 \
         -n 10 \
         -R 10 \
@@ -23,16 +25,29 @@ describe "getting leases" do
         172.1.0.10`
 
       expect(db_client[:lease4].count).to eq(10)
+      expect(dhcp_offer_packet_content).to include(File.read("./spec/fixtures/expected_lease_options_ordinary.txt"))
+      expect(dhcp_offer_packet_content).not_to include("Option: (234) Private")
     end
   end
 
-  context "when MoJO devices with delivery optimisation enabled requesting IPs" do
-    let(:expected_lease_options) { File.read("./spec/fixtures/expected_lease_options.txt") }
-    let(:dhcp_offer_packet_content) { `tshark -r ./leases.pcap -V -T text` }
+  context "when Windows 10 devices with client class value of 'W10TEST' send DHCP requests" do
+    it "provides a lease with DHCP options from global options but overrides dns-name option from client class options" do
+      `perfdhcp -r 2 \
+        -n 3 \
+        -R 1 \
+        -b mac=00:0c:01:02:03:04 \
+        -d 2 \
+        -4 \
+        -o 77,57313054455354 \
+        -W 20000000 \
+        172.1.0.10`
 
-    before { Process.fork { `tshark -iany -f 'ip src 172.1.0.10 and udp port 67' -w ./leases.pcap -q -a packets:1` } }
+      expect(dhcp_offer_packet_content).to include(File.read("./spec/fixtures/expected_lease_options_client_class.txt"))
+    end
+  end
 
-    it "leases out a lease with device specific dhcp options" do
+  context "when Windows 10 devices with delivery optimisation enabled send DHCP requests" do
+    it "provides a lease with DHCP options from global options as well as an additional option: private option 234" do
       `perfdhcp -r 2 \
         -n 3 \
         -R 1 \
@@ -43,7 +58,7 @@ describe "getting leases" do
         -W 20000000 \
         172.1.0.10`
 
-      expect(dhcp_offer_packet_content).to include(expected_lease_options)
+      expect(dhcp_offer_packet_content).to include(File.read("./spec/fixtures/expected_lease_options_delivery_optimised.txt"))
     end
   end
 end
