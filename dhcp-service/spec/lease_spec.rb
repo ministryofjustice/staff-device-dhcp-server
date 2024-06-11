5ac79ebff779da1158d91cd75d5dbb6df6f0431d
James Green <james.green6@justice.gov.uk>
2024-06-11 15:44:56 +0100

    testing rspec

--- dhcp-service/spec/lease_spec.rb
+++ dhcp-service/spec/lease_spec.rb
@@ -14,37 +14,37 @@ describe "Kea server" do
 
   after { db_client.disconnect }
 
-  context "when ordinary dhcp clients send DHCP requests" do
-    it "provides 10 leases to 10 clients, leases persist in the DB and provides DHCP options from global options" do
-      `perfdhcp -r 2 \
-        -n 10 \
-        -R 10 \
-        -d 2 \
-        -4 \
-        -W 20000000 \
-        172.1.0.10`
-
-      expect(db_client[:lease4].count).to eq(10)
-      expect(dhcp_offer_packet_content).to include(File.read("./spec/fixtures/expected_lease_options_ordinary.txt"))
-      expect(dhcp_offer_packet_content).not_to include("Option: (234) Private")
-    end
-  end
-
-  context "when Windows 10 devices with client class value of 'W10TEST' send DHCP requests" do
-    it "provides a lease with DHCP options from global options but overrides dns-name option from client class options" do
-      `perfdhcp -r 2 \
-        -n 3 \
-        -R 1 \
-        -b mac=00:0c:01:02:03:04 \
-        -d 2 \
-        -4 \
-        -o 77,57313054455354 \
-        -W 20000000 \
-        172.1.0.10`
-
-      expect(dhcp_offer_packet_content).to include(File.read("./spec/fixtures/expected_lease_options_client_class.txt"))
-    end
-  end
+#   context "when ordinary dhcp clients send DHCP requests" do
+#     it "provides 10 leases to 10 clients, leases persist in the DB and provides DHCP options from global options" do
+#       `perfdhcp -r 2 \
+#         -n 10 \
+#         -R 10 \
+#         -d 2 \
+#         -4 \
+#         -W 20000000 \
+#         172.1.0.10`
+#
+#       expect(db_client[:lease4].count).to eq(10)
+#       expect(dhcp_offer_packet_content).to include(File.read("./spec/fixtures/expected_lease_options_ordinary.txt"))
+#       expect(dhcp_offer_packet_content).not_to include("Option: (234) Private")
+#     end
+#   end
+#
+#   context "when Windows 10 devices with client class value of 'W10TEST' send DHCP requests" do
+#     it "provides a lease with DHCP options from global options but overrides dns-name option from client class options" do
+#       `perfdhcp -r 2 \
+#         -n 3 \
+#         -R 1 \
+#         -b mac=00:0c:01:02:03:04 \
+#         -d 2 \
+#         -4 \
+#         -o 77,57313054455354 \
+#         -W 20000000 \
+#         172.1.0.10`
+#
+#       expect(dhcp_offer_packet_content).to include(File.read("./spec/fixtures/expected_lease_options_client_class.txt"))
+#     end
+#   end
 
   context "when Windows 10 devices with delivery optimisation enabled send DHCP requests" do
     it "provides a lease with DHCP options from global options as well as an additional option: private option 234" do
